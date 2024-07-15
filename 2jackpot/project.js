b334c19ec511550eeb57cc8632e8d5488903d8ea
LeandroLopezs <157986136+LeandroLopezs@users.noreply.github.com>
2024-07-15 16:57:35 -0300

    jackpot exercise

---
+++ 2jackpot/project.js
@@ -0,0 +1,45 @@
+// 1. Depositt some money
+// 2. Determine number of lines to bet on
+// 3. Collect a bet amount
+// 4. Spin the slot machine
+// 5. check if the user won
+// 6. give the user their winning
+// 7. play again
+
+const prompt = require("prompt-sync")();
+
+const deposit = () => {
+    while (true) {
+
+
+        const depositAmount = prompt("Enter a deposit amount: ")
+        const numberDepositAmount = parseFloat(depositAmount);
+
+        if (isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
+            console.log("Invalid deposit amount, try again.")
+        } else {
+            return numberDepositAmount;
+        }
+    }
+
+};
+
+const getNumberOfLines = () => {
+    while (true) {
+
+
+        const lines = prompt("Enter the number of lines to bet on (1-3): ")
+        const numberOfLines = parseFloat(lines);
+
+        if (isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines <= 3) {
+            console.log("Invalid deposit amount, try again.");
+        } else {
+            return numberOflines;
+        }
+    }
+}
+
+const depositAmount = deposit();
+console.log(depositAmount);
+
+20:06
