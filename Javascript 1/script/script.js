d9693036550d43987cb25cbacc44a0086197fc66
LeandroLopezs <157986136+LeandroLopezs@users.noreply.github.com>
2024-11-13 10:59:49 -0300

    sda

---
+++ Javascript 1/script/script.js
@@ -0,0 +1,8 @@
+
+
+const balance = 5000;
+const description = 'Salary';
+const newTransaction = true;
+
+const balanceNumberEl = document.getElementsByClassName('.balance-number');
+balanceNumberEl.textContent = 10000;
