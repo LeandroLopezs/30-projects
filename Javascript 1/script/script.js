e021cb576efa377afccaed170fcbee772366fb77
LeandroLopezs <157986136+LeandroLopezs@users.noreply.github.com>
2024-11-14 13:45:43 -0300

    site

--- Javascript 1/script/script.js
+++ Javascript 1/script/script.js
@@ -1,42 +1,10 @@
+const transactionsEl = document.querySelector('.transactions');
 
+transactionsEl.addEventListener('click', function (blabla) {
 
-/*const text = 'Hello';
-const salary = 2000;
-const isExpense = false;
-const expenses = [1000, 30, 25];
-const transaction = {
-    description: 'Salary',
-    amount: 5000
-};*/
-//function declaration
-/*function calculateBalance(income, expenses) {
-    const balance = income - expenses;
-    return balance;
-};*/
-//function expression
-/*
-function calculateBalance(income, expenses) {
-    const balance = income - expenses;
-    return balance;
-};
-*/
-//arrow function
-/*const calculateBalance = (income) => income - 500;
+    console.log(blabla.target);
 
-
-const result = calculateBalance(5000);
-console.log(result);*/
-//const balanceEl = document.querySelector('.balance-number');
-
-//balanceEl.insertAdjacentHTML('beforeend', '<span>2000</span>');
-
-//balanceEl.addEventListener('click', () => {
-// balanceEl.classList.add('balance-number--special')
-//});
-
-//balanceEl.innerHTML = '<span class="special">Unavailable</span>';
-
-
+});
 
 
 
