0ae3f7bed5b4cf83398b904d9bdb2e8007fd7bb2
LeandroLopezs <157986136+LeandroLopezs@users.noreply.github.com>
2024-11-15 10:48:16 -0300

    calculadora html e style

---
+++ CalculadoraMC/script/script.js
@@ -0,0 +1,5 @@
+/*
+1. Calculadora Básica
+Objetivo: Criar uma calculadora simples para operações matemáticas básicas como soma, subtração, multiplicação e divisão.
+O que aprender: Manipulação de eventos, operações matemáticas e manipulação do DOM.
+*/
