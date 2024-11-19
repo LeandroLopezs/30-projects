7f7707f364f11fb8ca5c87591f922d3a5481713c
LeandroLopezs <157986136+LeandroLopezs@users.noreply.github.com>
2024-11-19 12:56:57 -0300

    usarname

---
+++ JavaScript 3/script/script.js
@@ -0,0 +1,11 @@
+let userName = ` Leandro`;
+
+let result = userName.startsWith(" ");
+
+if (result) {
+    console.log("Your username can't begin with ' '")
+}
+else {
+    console.log(userName)
+}
+
