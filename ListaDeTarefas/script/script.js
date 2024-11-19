eb6239efaebfabc445fcd77a4a10ed0f6e9359db
LeandroLopezs <157986136+LeandroLopezs@users.noreply.github.com>
2024-11-19 16:45:54 -0300

    ssss

---
+++ ListaDeTarefas/script/script.js
@@ -0,0 +1,5 @@
+/*
+4. Lista de Tarefas (To-Do List)
+Objetivo: Criar uma lista onde o usuário pode adicionar, marcar como concluídas e excluir tarefas.
+O que aprender: Manipulação do DOM, uso de arrays para armazenar dados e eventos do teclado/mouse.
+*/
