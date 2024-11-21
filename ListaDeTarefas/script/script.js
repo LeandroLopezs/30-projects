91c87a7a54820cabeab612b1f48dd38aba264aef
LeandroLopezs <157986136+LeandroLopezs@users.noreply.github.com>
2024-11-21 13:53:47 -0300

    lista +ou-

--- ListaDeTarefas/script/script.js
+++ ListaDeTarefas/script/script.js
@@ -3,3 +3,13 @@
 Objetivo: Criar uma lista onde o usuário pode adicionar, marcar como concluídas e excluir tarefas.
 O que aprender: Manipulação do DOM, uso de arrays para armazenar dados e eventos do teclado/mouse.
 */
+
+
+
+function addToDo() {
+    myUl = document.getElementById("myUl");
+    myInput = document.getElementById("myInput");
+    const textoLista = `<br><input type="checkbox">${myInput.value}</input>`
+    myUl.insertAdjacentHTML('beforeend', textoLista);
+    
+}
