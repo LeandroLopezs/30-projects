7c49af7af5ffb994f10be58e7cf5c19fde6b43e7
LeandroLopezs <157986136+LeandroLopezs@users.noreply.github.com>
2024-11-22 12:37:12 -0300

    Update script.js

--- JavaScript 3/script/script.js
+++ JavaScript 3/script/script.js
@@ -1,39 +1,36 @@
-// NUMBER GUESSING GAME
+function add(x, y) {
+    let result = x + y;
+    return result;
+}
+function subtract(x, y) {
+    return x - y;
+}
+function multiply(x, y) {
+    return x * y;
+}
+function divide(x, y) {
+    return x / y;
+}
+function isEven(number) {
 
-const minNum = 50;
-const maxNum = 100;
-const answer = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
-
-let attempts = 0;
-let guess;
-let running = true;
-
-while (running) {
-
-    guess = window.prompt(`Guess a number between between ${minNum} - ${maxNum}`)
-    guess = Number(guess);
-
-    if (isNaN(guess)) {
-        window.alert("Please enter a valid number")
-    }
-    else if (guess < minNum || guess > maxNum) {
-        window.alert("Please enter a valid number");
+    if (number % 2 === 0) {
+        return true;
     }
     else {
-        attempts++;
-        if (guess < answer) {
-            window.alert("Too Low! Try Again!")
-        }
-        else if (guess > answer) {
-            window.alert("Too High! Try Again")
+        return false;
+    }
+}
+function isValidEmail(email) {
 
-        }
-        else {
-            window.alert(`CORRECT! The answer was ${answer}!52. It took you ${attempts} attempts!`)
-            running = false;
-        }
+    if (email.includes("@")) {
+        return true;
+    }
+    else {
+        return false;
     }
 
-
-    running = false;
+    
 }
+
+console.log(isValidEmail("fudido@outlook.com"))
+
