54db6732fc82ab983c62e82e28f257206040c63a
LeandroLopezs <157986136+LeandroLopezs@users.noreply.github.com>
2024-11-23 12:59:11 -0300

    conversor de temperatura

--- JavaScript 3/script/script.js
+++ JavaScript 3/script/script.js
@@ -1,36 +1,9 @@
-function add(x, y) {
-    let result = x + y;
-    return result;
-}
-function subtract(x, y) {
-    return x - y;
-}
-function multiply(x, y) {
-    return x * y;
-}
-function divide(x, y) {
-    return x / y;
-}
-function isEven(number) {
+let fruits = ["apple", "orange", "banana", "coconut"];
 
-    if (number % 2 === 0) {
-        return true;
-    }
-    else {
-        return false;
-    }
+for (let i = fruits.length; i >= 0; i--) {
+
+    console.log(fruits[i]);
+
+
 }
-function isValidEmail(email) {
-
-    if (email.includes("@")) {
-        return true;
-    }
-    else {
-        return false;
-    }
-
-    
-}
-
-console.log(isValidEmail("fudido@outlook.com"))
 
