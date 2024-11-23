c1a6e417412f39c5bc11165ad2704d64f9fcbc6c
LeandroLopezs <157986136+LeandroLopezs@users.noreply.github.com>
2024-11-23 09:11:20 -0300

    pedra papel tesoura

--- joKenPoMC/script/script.js
+++ joKenPoMC/script/script.js
@@ -1,1 +1,17 @@
-const choices = ['pedra', 'papel', 'tesoura'];
+
+const jogador = document.getElementById("score-player")
+const computador = document.getElementById("score-computer")
+
+function aleatorio() {
+    const choices = Boolean(Math.round(Math.random()));
+
+    if (choices == true) {
+        const points = " |";
+        jogador.insertAdjacentHTML('beforeend', points);
+
+    }
+    else {
+        const points = " |";
+        computador.insertAdjacentHTML('beforeend', points);
+    }
+}
