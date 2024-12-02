aaea1bc210b424550ef30306a68796b3457cba6f
LeandroLopezs <157986136+LeandroLopezs@users.noreply.github.com>
2024-12-02 10:25:03 -0300

    timer e relogio

--- Javascript 4/script/script.js
+++ Javascript 4/script/script.js
@@ -1,18 +1,12 @@
-let score = 0;
+function updateClock() {
 
-function increaseScore(points){
-    score += points;
-    console.log(`+${points}pts`);
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, 0);
+    const minutes = now.getMinutes().toString().padStart(2, 0);
+    const seconds = now.getSeconds().toString().padStart(2, 0);
+    const timeString = `${hours}:${minutes}:${seconds}`;
+    document.getElementById('clock').textContent = timeString;
 }
 
-function decreaseScore(points){
-    score -= points;
-    console.log(`-${points}pts`);
-}
-
-function getScore(){
-    return score;
-}
-
-increaseScore(5);
-increaseScore(6);
+updateClock();
+setInterval(updateClock, 1000);
