c341c930197e300d4964c6feee5de437fcbd085d
LeandroLopezs <157986136+LeandroLopezs@users.noreply.github.com>
2024-12-04 16:57:04 -0300

    celsius pra fahrenheit

---
+++ CelsiusFahrenheit/script/script.js
@@ -0,0 +1,8 @@
+farenheit = document.getElementById('farenheit');
+
+function calcular() {
+    celsius = document.getElementById('celsius').value
+    farenheit.textContent = "Farenheit: "
+    result = 9 / 5 * celsius + 32;
+    farenheit.insertAdjacentHTML('beforeend', result);
+} s
