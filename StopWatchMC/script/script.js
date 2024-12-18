745478bc02019485042fb93406048abceeea5065
LeandroLopezs <157986136+LeandroLopezs@users.noreply.github.com>
2024-12-18 07:57:41 -0300

    timer

--- StopWatchMC/script/script.js
+++ StopWatchMC/script/script.js
@@ -22,5 +22,30 @@ function start() {
 
 function formatTime(elapsedTime) {
     const milliseconds = Math.floor((elapsedTime % 1000) / 10);
-    const seconds = Math.floor()
+    const seconds = Math.floor((elapsedTime % (1000 * 60)) / 1000);
+    const minutes = Math.floor((elapsedTime % (1000 * 60 * 60)) / (1000 * 60));
+    const hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+    return (
+        (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" +
+        (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" +
+        (seconds ? (seconds > 9 ? seconds : "0" + seconds) : "00") + "." +
+        (milliseconds > 9 ? milliseconds : "0" + milliseconds)
+    );
 }
+
+function stop() {
+    clearInterval(timerInterval);
+    startButtonEl.disabled = false;
+    stopButtonEl.disabled = true;
+}
+
+function reset() {
+    clearInterval(timerInterval);
+
+    elapsedTime = 0;
+    timerEl.textContent = "00:00:00"
+
+    startButtonEl.disabled = false;
+    stopButtonEl.disabled = true;
+}
+
